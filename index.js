5aac2feb10e7aba0f112fc379121ca2a3345f7db
Aida Kvrgić <kvrgicaida95@gmail.com>
2025-06-17 15:48:05 +0200

    fix: update frontend url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json({limit: '10mb'}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://crpit-frontend.vercel.app/",
   credentials: true,
 }))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
